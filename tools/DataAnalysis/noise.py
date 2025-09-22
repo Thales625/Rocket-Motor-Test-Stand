d4686bd51e57a8c1f9206732c38a999717c4c0f2
thales <thaleshend@gmail.com>
2025-09-22 11:41:36 -0300

    add noise analysis tool

--- tools/DataAnalysis/noise.py
+++ tools/DataAnalysis/noise.py
@@ -1,3 +1,9 @@
+def index_of(array, value):
+	for i, v in enumerate(array):
+		if v >= value:
+			return i
+	return None
+
 if __name__ == "__main__":
 	import argparse
 	from pathlib import Path
@@ -54,24 +60,33 @@ if __name__ == "__main__":
 	# plt.show()
 
 	# value process
-	t0 = 16000
-	t1 = 36500
+	t0 = 50000
+	t1 = times[-1]
 
-	v0 = np.interp(t0, times, values)
-	v1 = np.interp(t1, times, values)
+	i0 = index_of(times, t0)
+	i1 = index_of(times, t1)
+
+	v0 = values[i0]
+	v1 = values[i1]
 
 	deriv = (v1-v0) / (t1-t0)
 
-	fixed_values = [values[i]-deriv*(times[i]-t0) - v0 for i in range(len(times))]
+	times = times[i0:i1]
+	values = values[i0:i1]
+
+	values = [values[i]-deriv*(times[i]-t0) - v0 for i in range(len(times))]
+
+	mean = np.mean(values)
+	var = np.sum((values - mean)**2) / (len(values)-1)
+
+	print("Mean: ", mean)
+	print("Min: ", np.min(values))
+	print("Max: ", np.max(values))
+	print("Var: ", var)
 
 	# plot
-	plt.plot(times, values, label="Raw data")
-	# plt.plot(times, values, 'o', label="Raw data")
-	# plt.plot(times, fixed_values, label="Processed data")
-
-	# plt.axvline(t0, linestyle="dotted", c="r")
-	# plt.axvline(t1, linestyle="dotted", c="r")
-	# plt.axhline(v0, linestyle="dotted", c="r")
+	plt.plot(times, values, label="data")
+	plt.plot(times[:-1], np.diff(values), label="deriv")
 
 	plt.xlabel(r"Time $\left[ms\right]$")
 	plt.ylabel(r"Sensor Reading $\left[\frac{N}{N}\right]$")
